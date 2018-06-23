e1a2a4b744056ec584685de13288a26acc97585f
leoMurtha <aka.murtha@gmail.com>
2018-06-23 14:25:58 -0300

    a

---
+++ insert.sql
@@ -0,0 +1,161 @@
+INSERT INTO LOCAL(NFANTASIA, AREA, LOTACAO, ALUGUEL, ENDERECO, TELEFONE, NPROPRIETARIO)
+    VALUES('Buffet Andramar', 400, 300, 20.000, 'Avenida Brasil, RJ, 1100', '(11)40028922', 'Antonio Nunes');
+
+INSERT INTO LOCAL(NFANTASIA, AREA, LOTACAO, ALUGUEL, ENDERECO, TELEFONE, NPROPRIETARIO)
+    VALUES('Buffet Demácia', 300, 200, 15.000, 'Rua das flores, SP, 140', '(11)32244000', 'Leonardo Teemo');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO EVENTO(NRO, TEMA, DATA, DESCRICAO, LOCAL, TIPO)
+    VALUES(0010, 'Classico', to_date('2018/01/29 20:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Casamento clássico', 'Buffet Andramar', 'Casamento');
+
+INSERT INTO EVENTO(NRO, TEMA, DATA, DESCRICAO, LOCAL, TIPO)
+    VALUES(0097, 'Anos 80', to_date('2019/02/20 19:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Formatura oitentista', 'Buffet Demácia', 'Formatura');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO CERIMONIA(NRO, RELIGIAO, MESTRE)
+    VALUES(0010, 'Catolica', 'Padre Brisola');
+
+INSERT INTO CERIMONIA(NRO, RELIGIAO, MESTRE)
+    VALUES(0097, NULL, NULL);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO COLACAO(NRO)
+    VALUES(0097);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO COMISSAO(NOME, TELEFONE, EMAIL)
+    VALUES('Comissão BCC 016', '(11)99999-8787', 'comissaobcc@usp.br');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO PESSOA(CPF, NOME, TIPO)
+    VALUES('444.333.000-55', 'Joao Pedro', 'Noivo');
+
+INSERT INTO PESSOA(CPF, NOME, TIPO)
+    VALUES('999.555.777-22', 'Pedro Santos', 'Formando');
+
+INSERT INTO PESSOA(CPF, NOME, TIPO)
+    VALUES('111.222.333-45', 'Monica Sanches', 'Funcionario');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO NOIVO(CPF, TELEFONE)
+    VALUES('444.333.000-55', '(11)998987676');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO FORMANDO(CPF, CURSO, INSTITUICAO)
+    VALUES('999.555.777-22', 'BCC', 'USP');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO SERVICO(ORDEM_SERVICO, PRECO, TIPO)
+    VALUES(050, 2000.00, 'Estilizacao');
+
+INSERT INTO SERVICO(ORDEM_SERVICO, PRECO, TIPO)
+    VALUES(040, 2500.00, 'Decoracao');
+
+INSERT INTO SERVICO(ORDEM_SERVICO, PRECO, TIPO)
+    VALUES(030, 1000.00, 'Florista');
+
+INSERT INTO SERVICO(ORDEM_SERVICO, PRECO, TIPO)
+    VALUES(020, 800.00, 'Seguranca');
+
+INSERT INTO SERVICO(ORDEM_SERVICO, PRECO, TIPO)
+    VALUES(010, 4000.00, 'Buffet');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO ESTILIZACAO(ORDEM_SERVICO, PACOTE, MODELO, QUANTIDADE)
+    VALUES(050, 'Completo', 'Camiseta', 100);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO FLORISTA(ORDEM_SERVICO)
+    VALUES(030);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO ARRANJO(ORDEM_SERVICO, NRO_ARRANJO)
+    VALUES(030, 05);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO ESPECIES(ORDEM_SERVICO, NOME_ESPECIE)
+    VALUES(030, 'Rosa');
+
+INSERT INTO ESPECIES(ORDEM_SERVICO, NOME_ESPECIE)
+    VALUES(030, 'Orquidea');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO SEGURANCA(ORDEM_SERVICO, NIVEL)
+    VALUES(020, 'Sofisticado');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO DECORACAO(ORDEM_SERVICO, TEMA)
+    VALUES(040, 'Classico');
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO OBJETO(ORDEM_SERVICO, NOME, QUANTIDADE)
+    VALUES(040, 'Mesa', 80);
+
+INSERT INTO OBJETO(ORDEM_SERVICO, NOME, QUANTIDADE)
+    VALUES(040, 'Cortinas', 30);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO BUFFET(ORDEM_SERVICO)
+    VALUES(010);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO CARDAPIO(ORDEM_SERVICO, NOME, INGREDIENTES, QUANTIDADE)
+    VALUES(010, 'Mini Salada', 'Alface, tomate, molho balsâmico', 300);
+
+INSERT INTO CARDAPIO(ORDEM_SERVICO, NOME, INGREDIENTES, QUANTIDADE)
+    VALUES(010, 'Canapé de salmão', 'Massa, salmão', 300);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO BEBIDA(ORDEM_SERVICO, NOME, QUANTIDADE)
+    VALUES(010, 'Refrigerante', 500);
+
+INSERT INTO BEBIDA(ORDEM_SERVICO, NOME, QUANTIDADE)
+    VALUES(010, 'Cerveja', 500);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO FUNCIONARIO(CPF, CARGO, ENDERECO, CT, SALARIO, ORDEM_SERVICO)
+    VALUES('111.222.333-45', 'Chefe de cozinha', 'Alameda A, 35', '222.22222.22-2', 30000.00, 010);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO FESTA_CASAMENTO(NRO, FLORISTA)
+    VALUES(0010, 030);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO FESTA_FORMATURA(NRO, ESTILIZACAO)
+    VALUES(0097, 050);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO FORMATURA(COMISSAO, FESTA, COLACAO)
+    VALUES('Comissão BCC 016', 0097, 0097);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO CASAMENTO(NOIVO, FESTA, CERIMONIA)
+    VALUES('444.333.000-55', 0010, 0010);
+
+------------------------------------------------------------------------------------------------------
+
+INSERT INTO FORMA(FORMANDO, COMISSAO)
+    VALUES('999.555.777-22', 'Comissão BCC 016');
--- oracledb.py
+++ oracledb.py
@@ -4,111 +4,81 @@ import time
 import cx_Oracle
 
 
-#loop para mostrar o menu
+# loop para mostrar o menu
 def menu():
-	while (True):
-		#limpa a tela toda vez que entrar no menu
-		os.system('clear')
-		#mostra as opcoes disponiveis
-		print('Selecione uma opcao:')
-		print('1) ')
-		print('2) ')
-		print('3) ')
-		print('4) ')
-		print('5) ')
-		print('6) Sair')
-		#le a opcao desejada
-		opcao = input()
-		#limpa a tela
-		os.system('clear')
+    while (True):
+        # limpa a tela toda vez que entrar no menu
+        os.system('clear')
+        # mostra as opcoes disponiveis
+        print('Selecione uma opcao:')
+        print('1) ')
+        print('2) ')
+        print('3) ')
+        print('4) ')
+        print('5) ')
+        print('6) Sair')
+        # le a opcao desejada
+        opcao = input()
+        # limpa a tela
+        os.system('clear')
 
-		if (opcao == 1):
-			print (opcao)
+        if (opcao == 1):
+            print(opcao)
 
-		elif (opcao == 2):
-			print (opcao)
+        elif (opcao == 2):
+            print(opcao)
 
-		elif (opcao == 3):
-			print (opcao)
+        elif (opcao == 3):
+            print(opcao)
 
-		elif (opcao == 4):
-			print (opcao)
+        elif (opcao == 4):
+            print(opcao)
 
-		elif (opcao == 5):
-			print (opcao)
+        elif (opcao == 5):
+            print(opcao)
 
-		elif (opcao == 6):
-			print (opcao)
-			exit()
-		else:
-			print('Opcao invalida. Selecione uma nova opcao.')
-			time.sleep(2)
-
-
-def get_all_rows(label, data_type='uniforme'):
-	#Querry all rows
-	cursor = connection.cursor()
-	print (cursor)
+        elif (opcao == 6):
+            print(opcao)
+            exit()
+        else:
+            print('Opcao invalida. Selecione uma nova opcao.')
+            time.sleep(2)
 
 
-dsn = cx_Oracle.makedsn('grad.icmc.usp.br', 15215, 'orcl')
-connection = cx_Oracle.connect(user='L4182085', password='041097l$', dsn=dsn)
+def connect():
+	dsn = cx_Oracle.makedsn('grad.icmc.usp.br', 15215, 'orcl')
+	
+	connection = cx_Oracle.connect(user='L4182085', password='041097l$', dsn=dsn)
 
-print (cx_Oracle.clientversion())
+	print(cx_Oracle.clientversion())
+	print('Connected to oracle DB...\n')
+	
 
-cursor = connection.cursor()
+	return connection
 
+def main():
+    
+	conn = connect()
+	
+	cursor = conn.cursor()
 
-#Select 1
-statament = 'select * from time'
-cursor.execute(statament)
-response = cursor.fetchall()
-print (len(response))
-for i in response:
-    print (i)
+	# Select 1
+	statament = 'select * from time'
+	cursor.execute(statament)
+	
+	time = {i[0]: [] for i in (cursor.description)}
+	
+	response = cursor.fetchall()
+	for i in response:
+		print(i)
+		time['NOME'].append(i[0])
+		time['ESTADO'].append(i[1])
+		time['TIPO'].append(i[2])
+		time['SALDO_GOLS'].append(i[3])
+	
+	
+	
+	conn.close()
 
-print ('-----------------------------')
-
-
-#Select 2 (Positional)
-#saldo_gols = input()
-saldo_gols = 20
-cursor.execute('SELECT * \
-                FROM time \
-                WHERE estado = :1 AND saldo_gols> :2', ('SP', saldo_gols))
-for i in cursor:
-    print (i)
-
-
-print ('-----------------------------')
-
-
-#Select 3 (Named)
-cursor.execute('SELECT * \
-                FROM time \
-                WHERE estado= :estado AND saldo_gols>= :saldo', {'estado':'SP', 'saldo':20})
-#dict =  {i[0]: None for i in cursor.description}
-#print dict
-for i in cursor:
-    print (i)
-
-
-print ('---------------------------------')
-
-
-#Juntando Select 1 com 3
-estado = 'SP'
-saldo = 20
-statament = ('SELECT * \
-                FROM time \
-                WHERE estado= :estado AND saldo_gols>= :saldo')
-cursor.execute(statament, {'estado':estado, 'saldo': saldo})
-
-dict =  {i[0]: None for i in cursor.description}
-print (dict)
-
-response = cursor.fetchall()
-print (response)
-cursor.close()
-
-connection.close()
+if __name__ == '__main__':
+    main()
